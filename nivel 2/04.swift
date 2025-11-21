9cca181d14ed0a5b4ec05428cb08bef834d5f0a6
vitto2 <vitor2071@gmail.com>
2025-11-21 19:30:44 -0300

    feat: implementa tarefa 04 nivel 2

---
+++ nivel 2/04.swift
@@ -0,0 +1,16 @@
+struct City {
+  var population: Int? 
+}
+
+struct Country {
+  var capital: City?
+}
+
+var myCountry: Country? = nil
+let populationCount = myCountry?.capital?.population ?? 0
+
+var brasil: Country? = Country(capital: City(population: nil))
+
+let populationBrasil = brasil?.capital?.population ?? 0
+
+print(populationBrasil)
