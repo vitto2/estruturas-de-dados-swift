3519f2eb0a443960669e5c3f248f2e72fac83816
Vitor Silva De Souza <vitor2071@gmail.com>
2025-11-21 14:36:25 -0300

    feat: cria parte da tarefa 03 nivel 2

---
+++ nivel 2/03.swift
@@ -0,0 +1,25 @@
+let oldFollows: Set<String> = [
+    "tech_vibes_br",
+    "gourmet_na_madrugada",
+    "viajanteurbano_",
+    "pixel_creator",
+    "fitness_ninja_21",
+    "diario_do_dev",
+    "arte_em_foco",
+    "musicaeentrelinhas",
+    "universo_pet_lovers",
+    "geekplanet_oficial"
+]
+let currentFollows: [String] = [
+     "tech_vibes_br",
+    "gourmet_na_madrugada",
+    "viajanteurbano_",
+    "pixel_creator",
+    "fitness_ninja_21",
+    "mandy_candy", 
+    "futvitor22", 
+]
+
+let currentFollowsFilter: Set<String> = Set(currentFollows)
+
+print(currentFollowsFilter)
